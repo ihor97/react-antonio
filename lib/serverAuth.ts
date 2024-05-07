42cabfd48b8ccd085b892617206d08c2cbb171c0
ihor97 <daniel.waffner@gmail.com>
2024-05-07 21:41:52 +0100

    first commit

---
+++ lib/serverAuth.ts
@@ -0,0 +1,24 @@
+import { getSession } from 'next-auth/react';
+import prismadb from '@/lib/prismadb';
+import { NextApiRequest } from "next";
+const serverAuth = async (req:NextApiRequest) => {
+    const session = await getSession({ req });
+
+    if (!session?.user?.email) {
+        throw new Error('Not signed in');
+    }
+
+    const currentUser = await prismadb.user.findUnique({
+        where: {
+            email: session.user.email
+        }
+    });
+
+    if (!currentUser) {
+        throw new Error('User not found');
+    }
+
+    return { currentUser };
+};
+
+export default serverAuth;
